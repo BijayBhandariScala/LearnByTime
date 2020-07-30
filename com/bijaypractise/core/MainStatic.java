d75a391d242260199740f4f33af0c4e487b6dbb7
Bijay Bhandari <bijay.bhandari007@gmail.com>
2020-07-30 14:58:32 -0400

    Calling static method

--- com/bijaypractise/core/MainStatic.java
+++ com/bijaypractise/core/MainStatic.java
@@ -5,7 +5,7 @@ public class MainStatic {
 	public static void main(String[] args) {
 		
 		System.out.println("Output:" +Static.a);
-
+        Static.getStatic();
 	}
 
 }
--- com/bijaypractise/core/Static.java
+++ com/bijaypractise/core/Static.java
@@ -4,6 +4,13 @@ public class Static {
 	
 	static int a= 10;
 	String str = "Bijay";
+	
+	static String getStatic() {
+		
+		System.out.println("from Ststic method: "+ a);
+		return null;
+		
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello Bijay!!" + a );
