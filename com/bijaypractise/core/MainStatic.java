b388c374da12e0f8ce8df081faa0595cfd4d0a70
Bijay Bhandari <bijay.bhandari007@gmail.com>
2020-07-30 14:35:20 -0400

    Added main class to use static variable
    -it cannot be final

---
+++ com/bijaypractise/core/MainStatic.java
@@ -0,0 +1,11 @@
+package com.bijaypractise.core;
+
+public class MainStatic {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Output:" +Static.a);
+
+	}
+
+}
--- com/bijaypractise/core/Static.java
+++ com/bijaypractise/core/Static.java
@@ -1,9 +1,13 @@
 package com.bijaypractise.core;
 
 public class Static {
+	
+	static int a= 10;
+	String str = "Bijay";
 
 	public static void main(String[] args) {
-		System.out.println("Hello Bijay!!");
+		System.out.println("Hello Bijay!!" + a );
+		
 	}
 
 }
