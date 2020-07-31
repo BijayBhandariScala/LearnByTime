da96851e5dc63e3a5810a6029d1bc5d8b1402c74
Bijay Bhandari <bijay.bhandari007@gmail.com>
2020-07-31 10:29:37 -0400

    using StringBuffer to add char.

--- com/bijaypractise/core/StringSwap.java
+++ com/bijaypractise/core/StringSwap.java
@@ -4,18 +4,19 @@ public class StringSwap {
 
 	public static void main(String[] args) {
 		String text = "goose"; // Add A in the front and replace o with A.
-		System.out.println(getswap(text));
+        System.out.println(getswap(text));
 
 	}
 
-	/*public static String getswap(String text) {
+	public static String getswap(String text) {
 
-		StringBuilder sb  = new StringBuilder();
-		sb.replace(0, 0, "A");
+		//char[] str = text.toCharArray();
+		StringBuilder sb  = new StringBuilder(text);
+		sb.insert(0,'A');
 
 		return sb.toString();
 
 
-	}*/
+	}
 
 }
