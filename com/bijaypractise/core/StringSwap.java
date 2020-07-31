f9a7ac99c8bcfe7564dc173f22c0f0102464fa67
Bijay Bhandari <bijay.bhandari007@gmail.com>
2020-07-31 09:59:13 -0400

    Added String exercise class

---
+++ com/bijaypractise/core/StringSwap.java
@@ -0,0 +1,21 @@
+package com.bijaypractise.core;
+
+public class StringSwap {
+
+	public static void main(String[] args) {
+		String text = "goose"; // Add A in the front and replace o with A.
+		System.out.println(getswap(text));
+
+	}
+
+	/*public static String getswap(String text) {
+
+		StringBuilder sb  = new StringBuilder();
+		sb.replace(0, 0, "A");
+
+		return sb.toString();
+
+
+	}*/
+
+}
