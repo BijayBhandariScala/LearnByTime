74c52a90ad00d26226bc060993090e813058bfc0
Bijay Bhandari <bijay.bhandari007@gmail.com>
2020-08-02 17:02:55 -0400

    Initial commit for finding duplicate array value

---
+++ com/bijaypractise/core/DuplicateInArray.java
@@ -0,0 +1,21 @@
+package com.bijaypractise.core;
+
+import java.util.Set;
+
+public class DuplicateInArray {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 5, 5, 6, 7}; //Find the duplicate array values.
+        System.out.println();
+
+    }
+
+    public Set<Integer> getDuplicatesArray(){
+
+
+
+
+        return ;
+    }
+}
--- com/bijaypractise/core/StringSwap.java
+++ com/bijaypractise/core/StringSwap.java
@@ -3,18 +3,26 @@ package com.bijaypractise.core;
 public class StringSwap {
 
 	public static void main(String[] args) {
-		String text = "goose"; // Add A in the front and replace o with A.
-        System.out.println(getswap(text));
+		String text = "gboose"; // Add A in the front and replace o with A.
+        System.out.println(getSwap(text));
 
 	}
 
-	public static String getswap(String text) {
+	public static String getSwap(String text) {
 
-		//char[] str = text.toCharArray();
+		char[] str = text.toCharArray();
 		StringBuilder sb  = new StringBuilder(text);
-		sb.insert(0,'A');
 
-		return sb.toString();
+        for (char c: str) {
+            String string=Character.toString(c);
+            if(string=="" || string.equals("o")){
+                sb.append("A");
+
+            }
+
+        }
+
+        return sb.toString();
 
 
 	}
