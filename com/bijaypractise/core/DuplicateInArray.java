a9d8e0dd3be4fad8881c6f62e3bc4433bcd26dcc
Bijay Bhandari <bijay.bhandari007@gmail.com>
2020-08-02 18:13:22 -0400

    Printing duplicates elements in aaray

--- com/bijaypractise/core/DuplicateInArray.java
+++ com/bijaypractise/core/DuplicateInArray.java
@@ -1,21 +1,29 @@
 package com.bijaypractise.core;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class DuplicateInArray {
 
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 5, 5, 6, 7}; //Find the duplicate array values.
-        System.out.println();
+        int[] array = {1, 2, 3, 5, 5, 6, 7}; //Find the duplicate element values.
+        System.out.println("Duplicate Elements are: "+ getDuplicatesArray(array));
 
     }
 
-    public Set<Integer> getDuplicatesArray(){
+    public static Set<Integer> getDuplicatesArray(int[] input){
 
+        Set<Integer> dup_val= new HashSet<>();
 
+        for(int i=0;i<input.length;i++){
+            for(int j=1;j<input.length;j++){
+                if(input[i]==input[j] && i!=j){
+                    dup_val.add(input[i]);
+                }
+            }
+        }
 
-
-        return ;
+        return dup_val ;
     }
 }
