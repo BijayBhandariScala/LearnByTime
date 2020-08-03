881483841ab9f59445af18e6cc3ff6a9765201d2
Bijay Bhandari <bijay.bhandari007@gmail.com>
2020-08-03 13:24:20 -0400

    String replace some character using StringBuffer

--- com/bijaypractise/core/StringReplace.java
+++ com/bijaypractise/core/StringReplace.java
@@ -2,36 +2,38 @@ package com.bijaypractise.core;
 
 public class StringReplace {
 
-	/*public static void main(String[] args) {
-		String text = "goose"; // Add A in the front and replace o with A.
-        System.out.println(getReplace(text));
+	/*You are given a string text consisting of English letters and spaces. Find all the
+        capital and lowercase Fs in it and insert a capital K right before each one of them.
+    */
 
-	}
+    /* Using StringBuffer */
+	public static String getFs(String text) {
 
-	public static String getReplace(String text) {
-
-		char[] str = text.toCharArray();
-		StringBuilder sb  = new StringBuilder(text);
-
-        for (char ch1 : str) {
-               String c= Character.toString(ch1);
-            if(c.contains("o")){
-                sb.append(c);
+		char textinput[] = text.toCharArray();
+		StringBuffer sb  = new StringBuffer();
+        for (char ch : textinput){
+            if(ch=='F' || ch=='f'){
+                sb.append('K');
             }
-
+            sb.append(String.valueOf(ch));
         }
         return sb.toString();
-
-	}*/
+	}
 
     public static void main(String[] args) {
         String str1 = "abcaadefaaghiaakla";
         String result = remove_Character(str1);
         System.out.println("\nOriginal string:");
         System.out.println(str1);
-        System.out.println("\nSecond string:");
+        System.out.println("\nSecond string:\n");
         System.out.println(result);
+
+        String text = "gooFssfse";
+        System.out.println(getFs(text));
+
     }
+
+    /* Using StringBuilder*/
     public static String remove_Character(String str1) {
         StringBuilder sb = new StringBuilder();
         char[] chArray = str1.toCharArray();
