8b247ce29bb3257bf0da391e88ad0ff69c59ad0b
Bijay Bhandari <bijay.bhandari007@gmail.com>
2020-08-04 08:06:16 -0400

    Sort integer array.

--- com/bijaypractise/core/SortArray.java
+++ com/bijaypractise/core/SortArray.java
@@ -2,11 +2,13 @@ package com.bijaypractise.core;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SortArray {
 
     /* Sorting Array
     * int[]
+    * Ascending
     * */
     public static void main(String[] args) {
 
@@ -14,6 +16,11 @@ public class SortArray {
         System.out.println("Before :" + Arrays.toString(intArray));
         Arrays.sort(intArray);
         System.out.println("After :" +Arrays.toString(intArray));
+
+        /* Descending order*/
+        Integer[] intArray1= {0,10,2,30,4,5,9,-1,8,45,67, -7,406,673,-5,0,1};
+        Arrays.sort(intArray1,Collections.reverseOrder());
+        System.out.println("descending order :" + Arrays.toString(intArray1));
 
     }
 }
