f6d0225178f21f362cc692c8fd9d345337d978b8
bijay <bijay.bhandari007@gmail.com>
2020-08-27 11:31:02 -0400

    Added non-static method

--- com/bijay/core/java/Static.java
+++ com/bijay/core/java/Static.java
@@ -3,13 +3,16 @@ package com.bijay.core.java;
 public class Static {
 	
 	static int a= 10;
-	String str = "Bijay";
+	String str = "Hello Bijay";
 	
 	static String getStatic() {
-		
-		System.out.println("from Ststic method: "+ a);
+		System.out.println("from Static method: "+ a);
 		return null;
-		
+	}
+
+	/*Non-static method*/
+	public void getData(){
+		System.out.println("I am here non-static method.");
 	}
 
 	public static void main(String[] args) {
